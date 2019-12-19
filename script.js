16dd09101a9b79619420fd2cd28b57ed6de56e12
Sally Erisman <sally.erisman@gmail.com>
2019-12-19 20:33:51 +0100

    Created function for storing user input. Created event listener.

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+const searchForm = document.querySelector("#search-form");
+const userInput = document.querySelector('#user-input');
 
+let userSearch;
+
+// Function for getting and storing user input, and then emptying the search field:
+const getSearch = function() {
+    userSearch = userInput.value;
+    userInput.value = "";
+    return userSearch;
+};
+
+searchForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    getSearch();
+});
 
 
 
